c821b95c05df3974b06f0e000ac16a6c98b833f0
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-03 12:09:44 +0200

    feat(level): add tree density style

--- src/level/tile/tree.ts
+++ src/level/tile/tree.ts
@@ -21,7 +21,61 @@ export class Tree extends Tile {
     const sx = 2 * 8;
     const sy = 9 * 8;
 
-    if (!level.onTopTiles.find((tile) => tile.x === x && tile.y === y)) {
+    const u = level.getTile(x, y - 1).id === this.id;
+    const l = level.getTile(x - 1, y).id === this.id;
+    const r = level.getTile(x + 1, y).id === this.id;
+    const d = level.getTile(x, y + 1).id === this.id;
+    const ul = level.getTile(x - 1, y - 1).id === this.id;
+    const ur = level.getTile(x + 1, y - 1).id === this.id;
+    const dl = level.getTile(x - 1, y + 1).id === this.id;
+    const dr = level.getTile(x + 1, y + 1).id === this.id;
+
+    if (u && ul && l) {
+      level.renderTile(map2d, sx + 2 * 8, sy, x, y, xOffset, yOffset);
+    } else {
+      level.renderTile(map2d, sx, sy, x, y, xOffset, yOffset);
+    }
+    if (u && ur && r) {
+      level.renderTile(
+        map2d,
+        sx + 2 * 8,
+        sy + 1 * 8,
+        x,
+        y,
+        xOffset + 8,
+        yOffset,
+      );
+    } else {
+      level.renderTile(map2d, sx + 1 * 8, sy, x, y, xOffset + 8, yOffset);
+    }
+    if (d && dl && l) {
+      level.renderTile(
+        map2d,
+        sx + 2 * 8,
+        sy + 1 * 8,
+        x,
+        y,
+        xOffset,
+        yOffset + 8,
+      );
+    } else {
+      level.renderTile(map2d, sx, sy + 1 * 8, x, y, xOffset, yOffset + 8);
+    }
+    if (d && dr && r) {
+      level.renderTile(map2d, sx + 2 * 8, sy, x, y, xOffset + 8, yOffset + 8);
+    } else {
+      level.renderTile(
+        map2d,
+        sx + 1 * 8,
+        sy + 1 * 8,
+        x,
+        y,
+        xOffset + 8,
+        yOffset + 8,
+      );
+    }
+
+    /*if (!level.onTopTiles.find((tile) => tile.x === x && tile.y === y)) {
       level.onTopTiles.push({ sx, sy, x, y, xExtraOffset: 0, yExtraOffset: 0 });
       level.onTopTiles.push({
         sx: sx + 8,
@@ -31,9 +85,9 @@ export class Tree extends Tile {
         xExtraOffset: 8,
         yExtraOffset: 0,
       });
-    }
-    level.renderTile(map2d, sx, sy + 8, x, y, xOffset, yOffset + 8);
-    level.renderTile(map2d, sx + 8, sy + 8, x, y, xOffset + 8, yOffset + 8);
+    }*/
+    //level.renderTile(map2d, sx, sy + 8, x, y, xOffset, yOffset + 8);
+    //level.renderTile(map2d, sx + 8, sy + 8, x, y, xOffset + 8, yOffset + 8);
   }
 
   public getOutcome(): TileOutcome {
